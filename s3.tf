646e39e5f9eccdcab3c00e3896e46fef8f8bbeac
Brendan Hosking <brendan.hosking@csiro.au>
2024-09-23 16:02:26 +1000

    FIX: Replace deprecated s3 lifecycle block

--- s3.tf
+++ s3.tf
@@ -1,10 +1,15 @@
 resource aws_s3_bucket prices_bucket {
   bucket_prefix = "petrol-prices-bucket"
   force_destroy = true
-  lifecycle_rule {
-    enabled = true
-      expiration {
+}
+
+resource aws_s3_bucket_lifecycle_configuration prices_bucket {
+  bucket = aws_s3_bucket.prices_bucket.id
+  rule {
+    id = "rule-1"
+    expiration {
       days = 1
     }
+    status = "Enabled"
   }
 }
